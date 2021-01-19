a5574248532227e393c3de4a58f931ce9e8b3d57
ALEKSEI.KVASOW <aleksei.kvasow@gmail.com>
2021-01-19 16:37:40 +0300

    add header

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,28 @@
-@@include("webpcheck.js")
+'use strict';
+
+const headerDropdown = document.querySelector('.dropdown');
+
+function toggleHeaderDropdown() {
+  headerDropdown.classList.toggle('active');
+}
+
+function closeHeaderDropdown() {
+  headerDropdown.classList.remove('active');
+}
+
+document.addEventListener('click', (e) => {
+  e.preventDefault();
+  const target = e.target;
+
+  if (
+    target.classList.contains('header__select') ||
+    target.closest('.header__select')
+  ) {
+    toggleHeaderDropdown();
+  } else if (
+    headerDropdown.classList.contains('active') &&
+    !target.closest('.dropdown')
+  ) {
+    closeHeaderDropdown();
+  }
+});
